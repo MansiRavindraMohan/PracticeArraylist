6740e6ea9a4950a5c0d3cf094dc0c506f2841533
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-09 09:55:23 +0530

    Add: Program for basic operations of array list

---
+++ PracticeArraylist/ArrayClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeArraylist
+{
+    public class ArrayClass
+    {
+        public void ArrayListPracticeDemo()
+        {
+            ArrayList arrayList = new ArrayList();
+            //Add
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add(4);
+            arrayList.Add(5);
+            arrayList.Add("Mansi");
+            //Insert
+            arrayList.Insert(0, 58);
+            arrayList.Insert(1, "Mohan");
+            //Remove
+            arrayList.Remove(1);
+            //RemoveAt
+            arrayList.RemoveAt(1);
+            //RemoveRange
+            arrayList.RemoveRange(0, 2);
+            int count = arrayList.Count;
+            //Foreach loop
+            foreach (var i in arrayList)
+            {
+                Console.WriteLine(i);
+            }
+            //For Loop
+            for (var i = 0; i < arrayList.Count; i++)
+            {
+                Console.WriteLine(arrayList[i]);
+            }
+        }
+    }
+}
---
+++ PracticeArraylist/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeArraylist
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayClass arrayClass = new ArrayClass();
+            arrayClass.ArrayListPracticeDemo();
+            Console.ReadLine();
+        }
+    }
+}
